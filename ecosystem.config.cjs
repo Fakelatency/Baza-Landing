e3174368c7600055a7262c2b413255a6c231aae5
Fakelatency <81415554+Fakelatency@users.noreply.github.com>
2026-09-25 11:00:30 +0200

    chore: set port to 3133

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,14 +3,14 @@ module.exports = {
     {
       name: "baza-landing",
       script: "node_modules/next/dist/bin/next",
-      args: "start -p 3001",
+      args: "start -p 3133",
       instances: 1,
       exec_mode: "fork",
       autorestart: true,
       max_memory_restart: "500M",
       env: {
         NODE_ENV: "production",
-        PORT: 3001,
+        PORT: 3133,
       },
     },
   ],
